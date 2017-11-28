f4d356dd1cc99311265efe3333311f3faeda7c3c
EDUARDO DE SOUSA PEREIRA LIMA <31049529812@jpmorgan.sp>
2017-11-28 18:47:27 -0200

    TOP!

---
+++ Atividade.cs
@@ -0,0 +1,13 @@
+namespace LAMBDA
+{
+    public abstract class Atividade
+    {
+        public static double subtrair(int v1,int v2){
+            return v1-v2;
+        }
+
+        public static string Multiplicar(int v1, int v2){
+            return "O resultado é "+(v1*v2);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace LAMBDA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Func<int,int,double> rs = Atividade.subtrair;
+            // System.Console.WriteLine(rs(10,5));
+            // Func<int,int,string> ser = Atividade.Multiplicar;
+            // System.Console.WriteLine(ser(10,5));
+
+            // Action<int,int> res = soma;
+            // res(10,5);
+
+            // Predicate<int> res = Par;
+            // System.Console.WriteLine(res(33));
+
+        //     Data dt = delegate(string msg){
+        //         return msg+"Hoje é "+DateTime.Now.ToString();
+        //     };
+        //     System.Console.WriteLine(dt("Bacon! "));
+        // }
+
+        // static void soma(int v1, int v2){
+        //     System.Console.WriteLine(v1+v2);;
+        // }
+
+        // static bool Par(int valor)
+        // {
+        //     return valor%2==0;
+        // }
+
+        // delegate string Data(string mensagem);
+        
+        int[] valores = {40,56,23,1,9,11,22};
+
+        // Func<int,bool> rs = delegate(int x){
+        //     return x%2==0;
+        // };
+
+        // Func<int,bool> rs = x => x % 2 == 0;
+
+
+        // foreach(int i in valores)
+        //     if(rs(i))
+        //         System.Console.WriteLine(i);
+
+        Func<double,double,double> calculo = (x,y) => x/y;
+        System.Console.WriteLine(calculo(23,5));
+
+        }
+
+    }
+}
